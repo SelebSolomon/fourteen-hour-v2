300e72ca44daf88bb534e0a78ededaa0fe5025f4
SelebSolomon <selebsolomon@gmail.com>
2025-12-12 02:23:06 +0100

    edit

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,7 +4,7 @@ export default () => ({
   secret: process.env.SECRET,
   dbHost: process.env.DB_HOST,
   dbPort: parseInt(process.env.DB_PORT!),
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   dbName: process.env.DB_NAME,
 });
