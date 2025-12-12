25d284d7008635f7b613fadb50bdbb70594d6cf4
SelebSolomon <selebsolomon@gmail.com>
2025-12-12 02:37:39 +0100

    edit

--- db/data-source.ts
+++ db/data-source.ts
@@ -86,7 +86,7 @@ import {
 //   DB_PASSWORD: process.env.DB_PASSWORD,
 //   DB_NAME: process.env.DB_NAME,
 // });
-// THIS IS USED IN APP MODULE FOR TYPEORM SETUPpp
+// THIS IS USED IN APP MODULE FOR TYPEORM SETUPp
 export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
